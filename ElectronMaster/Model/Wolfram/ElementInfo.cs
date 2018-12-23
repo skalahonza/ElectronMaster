899bcb6dc75484ea2a97fb17571e37e3d9df156d
Jan Skála <jan.skala@thenetw.org>
2018-12-23 20:19:57 +0100

    Gather wolfram info.

---
+++ ElectronMaster/Model/Wolfram/ElementInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NodaTime;
+
+namespace ElectronMaster.Model.Wolfram
+{
+    public class ElementInfo
+    {
+        public Element Element { get; set; }
+        public DateTime Discovery { get; set; }        
+    }
+
+    public class ElementWolframInfo
+    {
+        public int ProtonNumber { get; set; }
+        public string EnglishName { get; set; }
+        public LocalDateTime Discovery { get; set; }
+    }
+}
---
+++ ElectronMasterTests/Services/WolframServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using ElectronMaster.Model;
+using ElectronMaster.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ElectronMasterTests.Services
+{
+    [TestClass()]
+    public class WolframServiceTests
+    {
+       
+        [TestMethod()]
+        public async Task GetElementInfoTest()
+        {
+            var service = new WolframService();
+            var result = await service.GetElementInfo(new Element() { Electrons = 29 });
+            
+        }
+    }
+}
